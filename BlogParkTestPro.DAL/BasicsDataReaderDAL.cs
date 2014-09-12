509333ad7b258b4978106f9c74a5c4ea906e52a2
zhangchenghang <chenghang1937@126.com>
2014-09-12 17:40:22 +0800

    添加类

---
+++ BlogParkTestPro.DAL/BasicsDataReaderDAL.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlogParkTestPro.Model;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace BlogParkTestPro.DAL
+{
+    public class BasicsDataReaderDAL
+    {
+        DbHelperSQL helper = new DbHelperSQL();
+    }
+}
---
+++ BlogParkTestPro.WebUI/Controllers/BasicsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using BlogParkTestPro.Model;
+using BlogParkTestPro.WebUI.Models;
+using BlogParkTestPro.BLL;
+
+namespace BlogParkTestPro.WebUI.Controllers
+{
+    /// <summary>
+    /// 此控制器获取网站基础数据
+    /// </summary>
+    public class BasicsController : Controller
+    {
+        /// <summary>
+        /// 得到友情链接
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult ShowFriendLink()
+        {
+            return PartialView("");
+        }
+    }
+}
